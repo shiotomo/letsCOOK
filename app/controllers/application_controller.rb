09b6a9e895bd61623cddb7e03779731334b2f7e8
shiotomo <t.shiota16@gmail.com>
2017-08-15 19:07:01 +0000

    Devise add name

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,9 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.for(:sign_up) {|u| u.permit(:name, :email, :password, :password_confirmation, :admin)}
+    devise_parameter_sanitizer.for(:sign_in) {|u| u.permit(:name, :password)}
+    devise_parameter_sanitizer.for(:account_update) {|u| u.permit(:name, :email, :password, :password_confirmation, :admin)}
+  end
 end
