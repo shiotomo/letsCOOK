c55fb2158c19392a388557654e0fe53014c6c14f
shiotomo <t.shiota16@gmail.com>
2017-08-15 22:27:23 +0000

    Anable registrations controllers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
+  protected
   def configure_permitted_parameters
     devise_parameter_sanitizer.for(:sign_up) {|u| u.permit(:name, :email, :password, :password_confirmation, :admin)}
     devise_parameter_sanitizer.for(:sign_in) {|u| u.permit(:name, :password)}
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
-  devise_for :users
+  # devise_for :users
+  # devise_for :users, :controllers => { registrations: 'registrations' }
+  devise_for :users, controllers: {
+    registrations: 'users/registrations',
+    # passwords: 'users/passwords'
+  }
   get 'homes/index'
   get 'homes/about'
 
