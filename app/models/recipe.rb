f0085b449fcb9e0f06a0efde257ca8229e33530c
vitamiena <s13t210@stmail.eng.kagawa-u.ac.jp>
2017-08-30 21:44:55 +0900

    Add gem annotate

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,2 +1,14 @@
+# == Schema Information
+#
+# Table name: recipes
+#
+#  id         :integer          not null, primary key
+#  title      :string           not null
+#  user_id    :integer          not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  postdate   :date
+#
+
 class Recipe < ApplicationRecord
 end
