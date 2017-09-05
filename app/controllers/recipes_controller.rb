e16acaf334c3951bce8d3ca76b853ef77e380e0a
shiotomo <t.shiota16@gmail.com>
2017-09-05 16:52:07 +0000

    Update recipe view

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -30,36 +30,8 @@ class RecipesController < ApplicationController
   def destroy
   end
 
-  # def home
-  # end
+  def recipe
+    @recipe = Recipe.find(params[:data])
+  end
 
-  # def recipe 
-  # end
-
-  # def post
-  # end
-
-  # def list
-  #   @recipes = Recipe.where(user_id: current_user.id)
-  # end
-
-  # def show
-  # end
-
-  # def edit
-  #   @recipe = Recipe.all
-  # end
-
-  # def create
-  #   @recipe = Recipe.new
-  #   now = Time.current
-  #   @recipe.title = params[:recipe][:title]
-  #   @recipe.postdate = now
-  #   @recipe.user_id = current_user.id
-  #   @recipe.save
-  #   redirect_to '/recipes/list'
-  # end
-
-  # def new
-  # end
 end
